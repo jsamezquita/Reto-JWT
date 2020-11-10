4fb8b30f1c92c5c5794a2e23be46b8196154d0ee
Julian <js.amezquita@uniandes.edu.co>
2020-11-09 23:03:19 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainContent from './components/MainContent';
+import NavBar from './components/NavBar';
+
+const App = () => {
+    return(<div>
+        <NavBar/>
+        <MainContent/>
+        </div>)
+}
+
+export default App;
---
+++ src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainContent = () => {
+    return(
+        <div>
+            <h1>Main content</h1>
+            <p>Lorem ipsum...</p>
+        </div>
+    )
+}
+
+export default MainContent;
